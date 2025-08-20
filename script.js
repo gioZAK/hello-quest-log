99e7e42ac1af3334bd3dde148cf28d2aa3894d53
gioZAK <giovane.zakc@gmail.com>
2025-08-20 21:12:38 +0100

    Handle add button and enter: read, validate, and log quest input

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Quest Log JS Loaded');
 
     const questInput = document.getElementById('questInput');
-    const addQuestBtn = document.getElementsByName('addQuestBtn');
+    const addQuestBtn = document.getElementById('addQuestBtn');
     const questList = document.getElementById('questList');
 
     console.log({ questInput, addQuestBtn, questList});
+
+    addQuestBtn.addEventListener('click', () => {
+        const rawText = questInput.value;
+        const questText = rawText.trim();
+
+        if (questText === '') {
+            console.log('No quest entered.');
+            questInput.focus();
+            return;
+        }
+
+        console.log('New quest:', questText);
+        questInput.value = '';
+        questInput.focus()
+    });
+
+    questInput.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            addQuestBtn.click();
+        }
+    })
 } )
